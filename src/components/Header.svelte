072286e63f6004ef0ea23669c5e7e2ce05952bb3
Пользователь <Kalichak_eo@mail.ru>
2024-05-12 21:45:43 +0500

    Add dropdawn

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,7 +4,6 @@
     function toggleMenu() {
         isMenuOpen = !isMenuOpen;
     }
-    
 </script>
 
 <header>
@@ -17,25 +16,63 @@
     </button>
     <nav class:open={isMenuOpen}>
         <button class="close-button" on:click={toggleMenu}>
-            <img src="/img/arrowMenu.svg" alt="Закрыть">
+            <img src="/img/arrowMenu.svg" alt="Закрыть" />
         </button>
-        <ul class="ulMenu">
+        <div class="dropdown">
+            <button class="dropdown-button">
+                <img class="arrow" src="/img/icon/arrow.svg" alt="Arrow" />
+                <p class="maindropdowntext">Услуги печати</p>
+            </button>
+            <ul class="dropdown-list">
+                <li><p class="lastdropdowntext">Печать на баннере</p></li>
+                <li><p class="lastdropdowntext">Печать на сетке</p></li>
+                <li><p class="lastdropdowntext">Печать на самоклейке</p></li>
+                <li><p class="lastdropdowntext">Интерьерная печать</p></li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="dropdown-button">
+                <img class="arrow" src="/img/icon/arrow.svg" alt="Arrow" />
+                <p class="maindropdowntext">Услуги печати</p>
+            </button>
+            <ul class="dropdown-list">
+                <li><p class="lastdropdowntext">Печать на баннере</p></li>
+                <li><p class="lastdropdowntext">Печать на сетке</p></li>
+                <li><p class="lastdropdowntext">Печать на самоклейке</p></li>
+                <li><p class="lastdropdowntext">Интерьерная печать</p></li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <button class="dropdown-button">
+                <img class="arrow" src="/img/icon/arrow.svg" alt="Arrow" />
+                <p class="maindropdowntext">Услуги печати</p>
+            </button>
+            <ul class="dropdown-list">
+                <li><p class="lastdropdowntext">Печать на баннере</p></li>
+                <li><p class="lastdropdowntext">Печать на сетке</p></li>
+                <li><p class="lastdropdowntext">Печать на самоклейке</p></li>
+                <li><p class="lastdropdowntext">Интерьерная печать</p></li>
+            </ul>
+        </div>
+        <!-- <ul class="ulMenu">
+            <li></li>
+            <div class="block1"></div>
             <li>
-                <a href=""><h2>Контакты</h2></a>
+                <a href=""><h2>Портфолио</h2></a>
             </li>
             <div class="block1"></div>
             <li>
                 <a href=""><h2>Портфолио</h2></a>
             </li>
-        </ul>
+        </ul> -->
     </nav>
-    <img src="/img/logo_small.svg" alt="small logo" width="200" height="64">
+    <img src="/img/logo_small.svg" alt="small logo" width="200" height="64" />
     <ul class="ulHeader">
         <li>
             <a href=""><h2>Контакты</h2></a>
         </li>
         <div class="block1"></div>
-        <li>                
+        <li>
             <a href=""><h2>Портфолио</h2></a>
         </li>
     </ul>
@@ -47,22 +84,21 @@
         align-items: center;
         margin: 0;
         padding: 0% 5% 0%;
-        background: #E9E0CF;
+        background: #e9e0cf;
         justify-content: space-between;
-        height: 5em;
     }
 
     .block1 {
-        padding: 20px;
+        padding: 10px;
     }
-    
+
     nav {
         position: fixed;
         top: 0;
         left: 0;
         bottom: 0;
-        width: 300px;
-        background-color: #E9E0CF;
+        width: 400px;
+        background-color: #e9e0cf;
         transform: translateX(-100%);
         transition: transform 0.3s ease;
     }
@@ -85,21 +121,20 @@
         margin-left: auto;
         padding: 0;
     }
-    
+
     a {
         color: black;
         text-decoration: none;
     }
 
     a:hover {
-        color: #34828C;
+        color: #34828c;
         text-decoration: none;
     }
 
     h2 {
-        font-family: "Jost", sans-serif;
-        font-size: 35px;
-        font-weight: 400;
+        font-size: 1.1em;
+        font-weight: 500;
     }
 
     .burger-button {
@@ -131,10 +166,70 @@
     }
 
     .close-button {
-        background-color: #E9E0CF;
+        background-color: #e9e0cf;
         border: none;
         cursor: pointer;
         padding: 10px;
         font-size: 16px;
+    }
+
+    .dropdown {
+        position: relative;
+    }
+
+    .dropdown-button {
+        background-color: rgba(255, 255, 255, 0);
+        width: 100%;
+        height: 70px;
+        border: none;
+        padding: 10px 20px;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .arrow {
+        display: inline-block;
+        width: 15px;
+        height: 15px;
+        margin-right: 20px;
+        background-size: cover;
+        transform: rotate(90deg);
+        transition: transform 0.3s ease;
+    }
+
+    .dropdown-button:active,
+    .dropdown-button:focus {
+        background-color: rgba(255, 255, 255, 0.37);
+    }
+
+    .dropdown-list {
+        position: absolute;
+        top: 80%;
+        left: 20%;
+        right: 20%;
+        display: none;
+        list-style-type: none;
+    }
+
+    .dropdown-button:focus + .dropdown-list {
+        display: block;
+    }
+
+    .dropdown-button:focus .arrow {
+        transform: rotate(0deg);
+    }
+
+    .maindropdowntext{
+        font-size: 25px;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 500;
+    }
+
+    .lastdropdowntext {
+        font-size: 18px;
+        font-family: 'Jost', sans-serif;
+        font-weight: 400;
     }
 </style>
